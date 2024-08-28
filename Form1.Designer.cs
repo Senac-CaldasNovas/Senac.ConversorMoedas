6d0583500810d012c625e6ea74e06da3092052ec
Bruno Verçosa <69854207+brunoverc@users.noreply.github.com>
2024-08-27 21:47:31 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Senac.ConversorMoedas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            toolStrip1 = new ToolStrip();
+            label1 = new Label();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label3 = new Label();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            toolStripLabel1 = new ToolStripLabel();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 50);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.MediumTurquoise;
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(panel4);
+            panel2.Controls.Add(panel3);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 50);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(800, 400);
+            panel2.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.Bottom;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripLabel1 });
+            toolStrip1.Location = new Point(0, 425);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 25);
+            toolStrip1.TabIndex = 1;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.Location = new Point(326, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(149, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Converter Moedas";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Gainsboro;
+            panel3.Controls.Add(label4);
+            panel3.Controls.Add(textBox1);
+            panel3.Controls.Add(panel5);
+            panel3.Location = new Point(12, 78);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(300, 250);
+            panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Gainsboro;
+            panel4.Controls.Add(label5);
+            panel4.Controls.Add(textBox2);
+            panel4.Controls.Add(panel6);
+            panel4.Location = new Point(488, 78);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(300, 250);
+            panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.White;
+            panel5.Controls.Add(comboBox1);
+            panel5.Controls.Add(label2);
+            panel5.Controls.Add(pictureBox1);
+            panel5.Dock = DockStyle.Top;
+            panel5.Location = new Point(0, 0);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(300, 50);
+            panel5.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.White;
+            panel6.Controls.Add(comboBox2);
+            panel6.Controls.Add(pictureBox2);
+            panel6.Controls.Add(label3);
+            panel6.Dock = DockStyle.Top;
+            panel6.Location = new Point(0, 0);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(300, 50);
+            panel6.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(3, 7);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(40, 40);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label2.Location = new Point(49, 16);
+            label2.Name = "label2";
+            label2.Size = new Size(122, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Moeda origem";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(203, 16);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(83, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(202, 18);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(83, 23);
+            comboBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label3.Location = new Point(49, 16);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 21);
+            label3.TabIndex = 4;
+            label3.Text = "Moeda destino";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(3, 7);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(40, 40);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(375, 178);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 50);
+            button1.TabIndex = 2;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            textBox1.Location = new Point(17, 121);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(269, 29);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            textBox2.Location = new Point(16, 121);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(269, 29);
+            textBox2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.DimGray;
+            label4.Location = new Point(17, 223);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 2;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = Color.DimGray;
+            label5.Location = new Point(16, 223);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 3;
+            label5.Text = "label5";
+            // 
+            // toolStripLabel1
+            // 
+            toolStripLabel1.Name = "toolStripLabel1";
+            toolStripLabel1.Size = new Size(86, 22);
+            toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(toolStrip1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Senac - Conversor de Moedas";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private ToolStrip toolStrip1;
+        private Label label1;
+        private Panel panel4;
+        private Panel panel6;
+        private Panel panel3;
+        private Panel panel5;
+        private ComboBox comboBox2;
+        private PictureBox pictureBox2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Label label5;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox1;
+        private ToolStripLabel toolStripLabel1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Senac.ConversorMoedas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
