11b41f9f7990b074dc218955864e73c9a2ae8b29
Bruno Verçosa <69854207+brunoverc@users.noreply.github.com>
2024-09-02 21:45:29 -0300

    Imagens moedas

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,35 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panel1 = new Panel();
-            panel2 = new Panel();
-            toolStrip1 = new ToolStrip();
             label1 = new Label();
-            panel3 = new Panel();
+            panel2 = new Panel();
+            buttonConverter = new Button();
             panel4 = new Panel();
-            panel5 = new Panel();
+            labelValorMoedaDestino = new Label();
+            txtBoxValorDestino = new TextBox();
             panel6 = new Panel();
-            pictureBox1 = new PictureBox();
-            label2 = new Label();
-            comboBox1 = new ComboBox();
-            comboBox2 = new ComboBox();
+            comboBoxMoedaDestino = new ComboBox();
+            pictureBoxImagemDestino = new PictureBox();
             label3 = new Label();
-            pictureBox2 = new PictureBox();
-            button1 = new Button();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            label4 = new Label();
-            label5 = new Label();
+            panel3 = new Panel();
+            labelValorMoedaOrigem = new Label();
+            textBoxValorOrigem = new TextBox();
+            panel5 = new Panel();
+            comboBoxMoedaOrigem = new ComboBox();
+            label2 = new Label();
+            pictureBoxImagemOrigem = new PictureBox();
+            toolStrip1 = new ToolStrip();
             toolStripLabel1 = new ToolStripLabel();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
-            toolStrip1.SuspendLayout();
-            panel3.SuspendLayout();
             panel4.SuspendLayout();
-            panel5.SuspendLayout();
             panel6.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxImagemDestino).BeginInit();
+            panel3.SuspendLayout();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxImagemOrigem).BeginInit();
+            toolStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -69,28 +70,6 @@
             panel1.Size = new Size(800, 50);
             panel1.TabIndex = 0;
             // 
-            // panel2
-            // 
-            panel2.BackColor = Color.MediumTurquoise;
-            panel2.Controls.Add(button1);
-            panel2.Controls.Add(panel4);
-            panel2.Controls.Add(panel3);
-            panel2.Dock = DockStyle.Fill;
-            panel2.Location = new Point(0, 50);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(800, 400);
-            panel2.TabIndex = 0;
-            // 
-            // toolStrip1
-            // 
-            toolStrip1.Dock = DockStyle.Bottom;
-            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripLabel1 });
-            toolStrip1.Location = new Point(0, 425);
-            toolStrip1.Name = "toolStrip1";
-            toolStrip1.Size = new Size(800, 25);
-            toolStrip1.TabIndex = 1;
-            toolStrip1.Text = "toolStrip1";
-            // 
             // label1
             // 
             label1.AutoSize = true;
@@ -101,45 +80,61 @@
             label1.TabIndex = 0;
             label1.Text = "Converter Moedas";
             // 
-            // panel3
+            // panel2
             // 
-            panel3.BackColor = Color.Gainsboro;
-            panel3.Controls.Add(label4);
-            panel3.Controls.Add(textBox1);
-            panel3.Controls.Add(panel5);
-            panel3.Location = new Point(12, 78);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(300, 250);
-            panel3.TabIndex = 0;
+            panel2.BackColor = Color.MediumTurquoise;
+            panel2.Controls.Add(buttonConverter);
+            panel2.Controls.Add(panel4);
+            panel2.Controls.Add(panel3);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 50);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(800, 400);
+            panel2.TabIndex = 0;
+            // 
+            // buttonConverter
+            // 
+            buttonConverter.Image = (Image)resources.GetObject("buttonConverter.Image");
+            buttonConverter.Location = new Point(375, 178);
+            buttonConverter.Name = "buttonConverter";
+            buttonConverter.Size = new Size(50, 50);
+            buttonConverter.TabIndex = 2;
+            buttonConverter.UseVisualStyleBackColor = true;
             // 
             // panel4
             // 
             panel4.BackColor = Color.Gainsboro;
-            panel4.Controls.Add(label5);
-            panel4.Controls.Add(textBox2);
+            panel4.Controls.Add(labelValorMoedaDestino);
+            panel4.Controls.Add(txtBoxValorDestino);
             panel4.Controls.Add(panel6);
             panel4.Location = new Point(488, 78);
             panel4.Name = "panel4";
             panel4.Size = new Size(300, 250);
             panel4.TabIndex = 1;
             // 
-            // panel5
+            // labelValorMoedaDestino
             // 
-            panel5.BackColor = Color.White;
-            panel5.Controls.Add(comboBox1);
-            panel5.Controls.Add(label2);
-            panel5.Controls.Add(pictureBox1);
-            panel5.Dock = DockStyle.Top;
-            panel5.Location = new Point(0, 0);
-            panel5.Name = "panel5";
-            panel5.Size = new Size(300, 50);
-            panel5.TabIndex = 0;
+            labelValorMoedaDestino.AutoSize = true;
+            labelValorMoedaDestino.ForeColor = Color.DimGray;
+            labelValorMoedaDestino.Location = new Point(16, 223);
+            labelValorMoedaDestino.Name = "labelValorMoedaDestino";
+            labelValorMoedaDestino.Size = new Size(38, 15);
+            labelValorMoedaDestino.TabIndex = 3;
+            labelValorMoedaDestino.Text = "label5";
+            // 
+            // txtBoxValorDestino
+            // 
+            txtBoxValorDestino.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            txtBoxValorDestino.Location = new Point(16, 121);
+            txtBoxValorDestino.Name = "txtBoxValorDestino";
+            txtBoxValorDestino.Size = new Size(269, 29);
+            txtBoxValorDestino.TabIndex = 3;
             // 
             // panel6
             // 
             panel6.BackColor = Color.White;
-            panel6.Controls.Add(comboBox2);
-            panel6.Controls.Add(pictureBox2);
+            panel6.Controls.Add(comboBoxMoedaDestino);
+            panel6.Controls.Add(pictureBoxImagemDestino);
             panel6.Controls.Add(label3);
             panel6.Dock = DockStyle.Top;
             panel6.Location = new Point(0, 0);
@@ -147,39 +142,24 @@
             panel6.Size = new Size(300, 50);
             panel6.TabIndex = 0;
             // 
-            // pictureBox1
+            // comboBoxMoedaDestino
             // 
-            pictureBox1.Location = new Point(3, 7);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(40, 40);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            comboBoxMoedaDestino.FormattingEnabled = true;
+            comboBoxMoedaDestino.Items.AddRange(new object[] { "Bitcoin", "Dólar", "Euro", "Iene", "Real" });
+            comboBoxMoedaDestino.Location = new Point(202, 18);
+            comboBoxMoedaDestino.Name = "comboBoxMoedaDestino";
+            comboBoxMoedaDestino.Size = new Size(83, 23);
+            comboBoxMoedaDestino.TabIndex = 5;
+            comboBoxMoedaDestino.SelectedIndexChanged += comboBoxMoedaDestino_SelectedIndexChanged;
             // 
-            // label2
+            // pictureBoxImagemDestino
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            label2.Location = new Point(49, 16);
-            label2.Name = "label2";
-            label2.Size = new Size(122, 21);
-            label2.TabIndex = 1;
-            label2.Text = "Moeda origem";
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(203, 16);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(83, 23);
-            comboBox1.TabIndex = 2;
-            // 
-            // comboBox2
-            // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(202, 18);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(83, 23);
-            comboBox2.TabIndex = 5;
+            pictureBoxImagemDestino.Location = new Point(3, 7);
+            pictureBoxImagemDestino.Name = "pictureBoxImagemDestino";
+            pictureBoxImagemDestino.Size = new Size(40, 40);
+            pictureBoxImagemDestino.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxImagemDestino.TabIndex = 3;
+            pictureBoxImagemDestino.TabStop = false;
             // 
             // label3
             // 
@@ -191,57 +171,86 @@
             label3.TabIndex = 4;
             label3.Text = "Moeda destino";
             // 
-            // pictureBox2
+            // panel3
             // 
-            pictureBox2.Location = new Point(3, 7);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(40, 40);
-            pictureBox2.TabIndex = 3;
-            pictureBox2.TabStop = false;
+            panel3.BackColor = Color.Gainsboro;
+            panel3.Controls.Add(labelValorMoedaOrigem);
+            panel3.Controls.Add(textBoxValorOrigem);
+            panel3.Controls.Add(panel5);
+            panel3.Location = new Point(12, 78);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(300, 250);
+            panel3.TabIndex = 0;
             // 
-            // button1
+            // labelValorMoedaOrigem
             // 
-            button1.Location = new Point(375, 178);
-            button1.Name = "button1";
-            button1.Size = new Size(50, 50);
-            button1.TabIndex = 2;
-            button1.UseVisualStyleBackColor = true;
+            labelValorMoedaOrigem.AutoSize = true;
+            labelValorMoedaOrigem.ForeColor = Color.DimGray;
+            labelValorMoedaOrigem.Location = new Point(17, 223);
+            labelValorMoedaOrigem.Name = "labelValorMoedaOrigem";
+            labelValorMoedaOrigem.Size = new Size(38, 15);
+            labelValorMoedaOrigem.TabIndex = 2;
+            labelValorMoedaOrigem.Text = "label4";
             // 
-            // textBox1
+            // textBoxValorOrigem
             // 
-            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            textBox1.Location = new Point(17, 121);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(269, 29);
-            textBox1.TabIndex = 1;
+            textBoxValorOrigem.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            textBoxValorOrigem.Location = new Point(17, 121);
+            textBoxValorOrigem.Name = "textBoxValorOrigem";
+            textBoxValorOrigem.Size = new Size(269, 29);
+            textBoxValorOrigem.TabIndex = 1;
             // 
-            // textBox2
+            // panel5
             // 
-            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            textBox2.Location = new Point(16, 121);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(269, 29);
-            textBox2.TabIndex = 3;
+            panel5.BackColor = Color.White;
+            panel5.Controls.Add(comboBoxMoedaOrigem);
+            panel5.Controls.Add(label2);
+            panel5.Controls.Add(pictureBoxImagemOrigem);
+            panel5.Dock = DockStyle.Top;
+            panel5.Location = new Point(0, 0);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(300, 50);
+            panel5.TabIndex = 0;
             // 
-            // label4
+            // comboBoxMoedaOrigem
             // 
-            label4.AutoSize = true;
-            label4.ForeColor = Color.DimGray;
-            label4.Location = new Point(17, 223);
-            label4.Name = "label4";
-            label4.Size = new Size(38, 15);
-            label4.TabIndex = 2;
-            label4.Text = "label4";
+            comboBoxMoedaOrigem.FormattingEnabled = true;
+            comboBoxMoedaOrigem.Items.AddRange(new object[] { "Bitcoin", "Dólar", "Euro", "Iene", "Real" });
+            comboBoxMoedaOrigem.Location = new Point(203, 16);
+            comboBoxMoedaOrigem.Name = "comboBoxMoedaOrigem";
+            comboBoxMoedaOrigem.Size = new Size(83, 23);
+            comboBoxMoedaOrigem.TabIndex = 2;
+            comboBoxMoedaOrigem.SelectedIndexChanged += comboBoxMoedaOrigem_SelectedIndexChanged;
             // 
-            // label5
+            // label2
             // 
-            label5.AutoSize = true;
-            label5.ForeColor = Color.DimGray;
-            label5.Location = new Point(16, 223);
-            label5.Name = "label5";
-            label5.Size = new Size(38, 15);
-            label5.TabIndex = 3;
-            label5.Text = "label5";
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label2.Location = new Point(49, 16);
+            label2.Name = "label2";
+            label2.Size = new Size(122, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Moeda origem";
+            // 
+            // pictureBoxImagemOrigem
+            // 
+            pictureBoxImagemOrigem.ErrorImage = null;
+            pictureBoxImagemOrigem.Location = new Point(3, 7);
+            pictureBoxImagemOrigem.Name = "pictureBoxImagemOrigem";
+            pictureBoxImagemOrigem.Size = new Size(40, 40);
+            pictureBoxImagemOrigem.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxImagemOrigem.TabIndex = 0;
+            pictureBoxImagemOrigem.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.Bottom;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripLabel1 });
+            toolStrip1.Location = new Point(0, 425);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 25);
+            toolStrip1.TabIndex = 1;
+            toolStrip1.Text = "toolStrip1";
             // 
             // toolStripLabel1
             // 
@@ -262,18 +271,18 @@
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             panel2.ResumeLayout(false);
-            toolStrip1.ResumeLayout(false);
-            toolStrip1.PerformLayout();
-            panel3.ResumeLayout(false);
-            panel3.PerformLayout();
             panel4.ResumeLayout(false);
             panel4.PerformLayout();
-            panel5.ResumeLayout(false);
-            panel5.PerformLayout();
             panel6.ResumeLayout(false);
             panel6.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxImagemDestino).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxImagemOrigem).EndInit();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -288,17 +297,17 @@
         private Panel panel6;
         private Panel panel3;
         private Panel panel5;
-        private ComboBox comboBox2;
-        private PictureBox pictureBox2;
+        private ComboBox comboBoxMoedaDestino;
+        private PictureBox pictureBoxImagemDestino;
         private Label label3;
-        private ComboBox comboBox1;
+        private ComboBox comboBoxMoedaOrigem;
         private Label label2;
-        private PictureBox pictureBox1;
-        private Button button1;
-        private Label label5;
-        private TextBox textBox2;
-        private Label label4;
-        private TextBox textBox1;
+        private PictureBox pictureBoxImagemOrigem;
+        private Button buttonConverter;
+        private Label labelValorMoedaDestino;
+        private TextBox txtBoxValorDestino;
+        private Label labelValorMoedaOrigem;
+        private TextBox textBoxValorOrigem;
         private ToolStripLabel toolStripLabel1;
     }
 }
